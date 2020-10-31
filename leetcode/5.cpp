44b951392575f8f916ffcadc5e885e767956beeb
ddd <ddd@ddddeMacBook-Air.local>
2020-10-31 15:50:44 +0800

    1

--- leetcode/5.cpp
+++ leetcode/5.cpp
@@ -11,7 +11,10 @@ public:
         string ans;
         for(int i = 0;i < s.size();i++){
             string a1  = exp(s,i,i);
-            string a2 = exp(s,i,i + 1);
+            string a2 = "";
+            if(s[i] == s[i + 1]){
+                a2 = exp(s,i,i+1);
+            }
             if(a1.size() > maxx){
                 maxx = a1.size();
                 ans = a1;
@@ -25,14 +28,15 @@ public:
     }
     string  exp(string s,int t1,int t2){
         int l = t1,r = t2;
-        while(l > 0 && r < s.size() && s[l] == s[r]){
+        
+        while(l >= 0 && r < s.size() && s[l] == s[r]){
             l--;r++;
         }
         int cnt = 0;
         string ans;
-        for(int i = l;i <= r;i++){
-            ans[cnt++] = s[i];
-        }
+        
+        ans.append(s,l + 1,r - 1 - l);
+        cout<<l + 1<<" "<<r - 1 - l<<ans<<endl;
         return ans;
     }
 };
